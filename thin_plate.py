2f1ecd36e30180ce016116ee88d3ea5e58e333ce
alex <alexw1605@gmail.com>
2023-11-17 19:51:29 -0500

    correct to my knowledge

--- thin_plate.py
+++ thin_plate.py
@@ -5,44 +5,57 @@ import numpy as np
 # matboard params
 thickness = 1.27          # mm
 E = 4000                  # MPa
-poisson = 0.2             # unitless
+poisson = 0.2             # unitles
+flange_width = 10         # mm
 
 def top_center():
     global thickness, E, poisson
     k = 4
-    t = bp_.param[0][2]
-    b = bp_.param[0][1]
+ 
+    t = bp_.param[5][2]
+    b = bp_.param[5][1] - 2 * flange_width
 
-    return (k * np.pi * E ) / (12*(1-poisson**2)) * (t/b)**2
+    print(t, b)
+
+    return (k * np.pi**2 * E ) / (12*(1-poisson**2)) * (t/b)**2
     
 def free_edge():
     global thickness, E, poisson
     k = 0.425
-    t = bp_.param[0][2]
-    b = bp_.param[0][1]
 
-    return (k * np.pi * E ) / (12*(1-poisson**2)) * (t/b)**2
+    t = bp_.param[5][2]
+    b = bp_.param[5][1]
+
+    print(t, b)
+
+    return (k * np.pi**2 * E ) / (12*(1-poisson**2)) * (t/b)**2
 
 def web():
     global thickness, E, poisson
     k = 6
-    t = bp_.param[1][2]
-    b = bp_.param[1][1]
 
-    return (k * np.pi * E ) / (12*(1-poisson**2)) * (t/b)**2
+    t = bp_.param[1][1]
+    b = bp_.param[1][2]
+
+    print(t, b)
+
+    return (k * np.pi**2 * E ) / (12*(1-poisson**2)) * (t/b)**2
 
 def shear():
     global thickness, E, poisson
     k = 5
-    t = bp_.param[0][2]
-    h = bp_.param[0][1]
-    a = 50               # to be optimized
 
-    return (k * np.pi * E ) / (12*(1-poisson**2)) * ((t/h)**2 + (t/a)**2)
+    t = bp_.param[5][2]
+    h = bp_.param[5][1]
+    a = 50                      # to be optimized
+
+    print(t, h, a)
+
+    return (k * np.pi**2 * E ) / (12*(1-poisson**2)) * ((t/h)**2 + (t/a)**2)
 
 if __name__ == "__main__":
-    print("Top Center (Mpa): " + str(top_center()))
-    print("Free Edge (Mpa): " + str(free_edge()))
-    print("Web (Mpa): " + str(web()))
-    print("Shear (Mpa): " + str(shear()))
+    print("Top Center (MPa): " + str(top_center()))
+    print("Free Edge (MPa): " + str(free_edge()))
+    print("Web (MPa): " + str(web()))
+    print("Shear (MPa): " + str(shear()))
 
