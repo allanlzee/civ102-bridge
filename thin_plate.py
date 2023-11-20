53bb300bcaa44b1e444c3664fc043845f7a4d9c1
alex <alexw1605@gmail.com>
2023-11-20 16:45:23 -0500

    joji

--- thin_plate.py
+++ thin_plate.py
@@ -8,14 +8,14 @@ E = 4000                  # MPa
 poisson = 0.2             # unitles
 flange_width = 10         # mm
 
-def top_center():
+def center():
     global thickness, E, poisson
     k = 4
  
     t = bp_.param[5][2]
     b = bp_.param[5][1] - 2 * flange_width
 
-    print(t, b)
+    print(k, t, b)
 
     return (k * np.pi**2 * E ) / (12*(1-poisson**2)) * (t/b)**2
     
@@ -24,9 +24,9 @@ def free_edge():
     k = 0.425
 
     t = bp_.param[5][2]
-    b = bp_.param[5][1]
+    b = flange_width
 
-    print(t, b)
+    print(k, t, b)
 
     return (k * np.pi**2 * E ) / (12*(1-poisson**2)) * (t/b)**2
 
@@ -37,7 +37,7 @@ def web():
     t = bp_.param[1][1]
     b = bp_.param[1][2]
 
-    print(t, b)
+    print(k, t, b)
 
     return (k * np.pi**2 * E ) / (12*(1-poisson**2)) * (t/b)**2
 
@@ -46,16 +46,16 @@ def shear():
     k = 5
 
     t = bp_.param[5][2]
-    h = bp_.param[5][1]
-    a = 50                      # to be optimized
+    h = bp_.param[5][1] - 2*flange_width
+    a = 1200                      # to be optimized
 
-    print(t, h, a)
+    print(k, t, h, a)
 
     return (k * np.pi**2 * E ) / (12*(1-poisson**2)) * ((t/h)**2 + (t/a)**2)
 
 if __name__ == "__main__":
-    print("Top Center (MPa): " + str(top_center()))
-    print("Free Edge (MPa): " + str(free_edge()))
+    print("Bottom Center (MPa): " + str(center()))
+    print("Overhangs (MPa): " + str(free_edge()))
     print("Web (MPa): " + str(web()))
-    print("Shear (MPa): " + str(shear()))
+    print("Top Center (MPa): " + str(shear()))
 
